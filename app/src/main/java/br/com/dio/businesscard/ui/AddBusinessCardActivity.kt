41d23f3d73154f96e8cb3bf2851840c4434db6ba
Rodrigo Pronunciati <rodpro@hotmail.com>
2022-02-09 11:19:32 -0300

    Adição de color picker

--- app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
@@ -1,5 +1,6 @@
 package br.com.dio.businesscard.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -8,6 +9,9 @@ import br.com.dio.businesscard.App
 import br.com.dio.businesscard.R
 import br.com.dio.businesscard.data.BusinessCard
 import br.com.dio.businesscard.databinding.ActivityAddBusinessCardBinding
+import com.flask.colorpicker.ColorPickerView
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder
+
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
@@ -36,6 +40,28 @@ class AddBusinessCardActivity : AppCompatActivity() {
             Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_SHORT).show()
             finish()
         }
+
+        binding.tieCor.setOnClickListener {
+ 
+            ColorPickerDialogBuilder
+                .with(this)
+                .setTitle("Choose color")
+                .initialColor(Color.parseColor(binding.tieCor.text?.toString()))
+                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                .density(12)
+                .setOnColorSelectedListener { selectedColor ->
+                    Toast.makeText(this, "onColorSelected: #" + Integer.toHexString(selectedColor), Toast.LENGTH_SHORT).show()
+                }
+                .setPositiveButton(
+                    "ok"
+                ) { dialog, selectedColor, allColors -> binding.tieCor.setText("#" + Integer.toHexString(selectedColor))}
+                .setNegativeButton(
+                    "cancel"
+                ) { dialog, which -> }
+                .build()
+                .show()
+        }
+
 
         binding.btnClose.setOnClickListener {
             finish()
